12e0340935c75a2539bf541ef3fad11f78fc0b60
“Yash <syash5551@gmail.com>
2024-09-03 03:14:24 +0530

    updated

---
+++ quiz_app/frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "../styles/index.scss";
+
+const Footer: React.FC = () => {
+
+  return (
+    <footer>
+      <span>Yash Singh</span>
+    </footer>
+  );
+};
+
+export default Footer;
